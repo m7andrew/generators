d7ffe0f12019f48b98dfad4a175abc3ac03df9e8
Andrew <m7andrew@icloud.com>
2024-03-27 19:25:58 -0700

    Added additional tests
    Basic tests for return statements, try expressions, and recursive generators.

--- tests/tests.rs
+++ tests/tests.rs
@@ -2,16 +2,64 @@
 use generators::*;
 
 
-#[generator]
-fn simple() -> i32 {
-	yield 1;
-	yield 2;
-	yield 3;
+#[test]
+fn basic() {
+
+	#[generator]
+	fn numbers() -> i32 {
+		yield 1;
+		yield 2;
+		yield 3;
+	}
+
+	let result: Vec<i32> = numbers().collect();
+	assert_eq!(result, vec![1, 2, 3]);
 }
 
 
 #[test]
-fn test_simple() {
-	let result: Vec<i32> = simple().collect();
+fn returns() {
+
+	#[generator]
+	fn numbers() -> i32 {
+		yield 1;
+		yield 2;
+		return 3;
+	}
+
+	let result: Vec<i32> = numbers().collect();
 	assert_eq!(result, vec![1, 2, 3]);
+}
+
+
+#[test]
+fn try_expression() {
+
+	fn two() -> Result<i32, i32> {
+		Err(2)
+	}
+
+	#[generator]
+	fn numbers() -> Result<i32, i32> {
+		yield Ok(1);
+		yield Ok(two()?);
+		yield Ok(3);
+	}
+
+	let result: Vec<Result<i32, i32>> = numbers().collect();
+	assert_eq!(result, vec![Ok(1), Err(2)]);
+}
+
+
+#[test]
+fn recursive() {
+
+	#[generator(boxed)]
+	fn fib(start: i32, acc: i32) -> i32 {
+		yield start + acc;
+		for n in fib(acc, start + acc) { yield n }
+	}
+
+	let result: Vec<i32> = fib(1, 1).take(3).collect();
+	assert_eq!(result, vec![2, 3, 5]);
 }
